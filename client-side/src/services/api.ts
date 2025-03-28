c9a5b83a82ca22a400c73673702ed2e10c101384
DamDat-coder <datquoc393@gmail.com>
2025-03-28 12:10:26 +0700

    FIX | Fix Login Register Logic | DamDat-coder

--- client-side/src/services/api.ts
+++ client-side/src/services/api.ts
@@ -1,6 +1,11 @@
-import { Product, MemberBenefit } from "../types";
+// api.ts
+import { Product, MemberBenefit,User } from "../types";
+
+// Định nghĩa kiểu dữ liệu cho User
+
 
 const API_BASE_URL = "https://67e3b0622ae442db76d1204c.mockapi.io/";
+const USER_API_BASE_URL = "https://67e0f65058cc6bf785238ee0.mockapi.io/";
 
 export async function fetchProducts(): Promise<Product[]> {
     try {
@@ -23,3 +28,16 @@ export async function fetchMemberBenefits(): Promise<MemberBenefit[]> {
         return [];
     }
 }
+
+export async function fetchUsers(): Promise<User[]> {
+    try {
+        const res = await fetch(`${USER_API_BASE_URL}/user`, { cache: "no-store" });
+        if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+        return await res.json();
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        return [];
+    }
+}
+
+export type { User };

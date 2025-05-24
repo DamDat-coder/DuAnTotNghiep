34b72923829db3039a5e84c9f12a04390e127309
congtran203 <tvcong2804@gmail.com>
2025-05-24 16:07:39 +0700

    BUILD | Build Profile Page | CongTran

---
+++ client-side/src/components/Profile/tabs/AddressTab.tsx
@@ -0,0 +1,8 @@
+export default function ProfileTab() {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold mb-4">ĐỊA CHỈ GIAO HÀNG</h1>
+      {/* Các input, form như bạn đã thiết kế */}
+    </div>
+  );
+}

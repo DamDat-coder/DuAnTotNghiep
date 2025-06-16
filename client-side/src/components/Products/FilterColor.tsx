f8b7e230205ff37a39b8124a6b44e07141df553b
DamDat-coder <datquoc393@gmail.com>
2025-06-16 11:41:42 +0700

    FIX | Fix Products Page Layout With New Data | DamDat-coder

--- client-side/src/components/Products/FilterColor.tsx
+++ client-side/src/components/Products/FilterColor.tsx
@@ -1,48 +1,41 @@
-// src/components/FilterColor.tsx
-import { ColorOption } from "@/types";
+import { ColorOption } from "@/types/filter";
 
 interface FilterColorProps {
-  selectedColors: string[];
-  handleColorChange: (colorValue: string) => void;
+  selectedColor: string | null;
+  setSelectedColor: (colorValue: string | null) => void;
 }
 
 const colorOptions: ColorOption[] = [
-  { value: "black", label: "Đen", color: "#000000" },
-  { value: "cyan", label: "Xanh da trời", color: "#87CEEB" },
-  { value: "red", label: "Đỏ", color: "#FF0000" },
-  { value: "white", label: "Trắng", color: "#FFFFFF" },
-  { value: "pink", label: "Hồng", color: "#FFC0CB" },
-  { value: "color", label: "Màu da", color: "#FAD2B6" },
-  { value: "brown", label: "Nâu", color: "#8B4513" },
+  { value: "Đen", label: "Đen", color: "#000000" },
+  { value: "Xám", label: "Xanh da trời", color: "#87CEEB" },
+  { value: "Đỏ", label: "Đỏ", color: "#FF0000" },
+  { value: "trắng", label: "Trắng", color: "#FFFFFF" },
+  // { value: "Đen", label: "Hồng", color: "#FFC0CB" },
+  // { value: "Trắng", label: "Màu da", color: "#FAD2B6" },
+  // { value: "Đỏ", label: "Nâu", color: "#8B4513" },
 ];
 
-export default function FilterColor({ selectedColors, handleColorChange }: FilterColorProps) {
+export default function FilterColor({ selectedColor, setSelectedColor }: FilterColorProps) {
   return (
     <div className="flex flex-col gap-4 border-b pb-4 mt-4">
       <h3 className="text-base font-bold">Màu sắc</h3>
       <div className="grid grid-cols-3 gap-5 mt-2 justify-items-center">
         {colorOptions.map((option) => (
-          <label
+          <button
             key={option.value}
+            onClick={() => setSelectedColor(option.value === selectedColor ? null : option.value)}
             className="flex w-full flex-col items-center text-sm cursor-pointer text-center"
           >
-            <input
-              type="checkbox"
-              value={option.value}
-              checked={selectedColors.includes(option.value)}
-              onChange={() => handleColorChange(option.value)}
-              className="hidden"
-            />
             <div
               className={`w-8 h-8 rounded-full border flex items-center justify-center ${
-                option.value === "white" ? "border-gray-600 border-1 border-solid" : "border-gray-400"
-              }`}
+                option.value === "white" ? "border-gray-600 border-2" : "border-gray-400"
+              } ${selectedColor === option.value ? "ring-2 ring-black" : ""}`}
               style={{ backgroundColor: option.color }}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className={`h-4 w-4 ${
-                  selectedColors.includes(option.value)
+                  selectedColor === option.value
                     ? option.value === "white"
                       ? "text-black"
                       : "text-white"
@@ -63,7 +56,7 @@ export default function FilterColor({ selectedColors, handleColorChange }: Filte
             <span className="text-[0.875rem] font-bold flex flex-nowrap">
               {option.label}
             </span>
-          </label>
+          </button>
         ))}
       </div>
     </div>

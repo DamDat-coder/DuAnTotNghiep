61f7129bc4d0dc2f30ad470d5485f60490e9a08a
tanha-coder <tannhutha04@gmail.com>
2025-06-26 21:04:40 +0700

    Tanha coder (#112)
    * ADD | Add Order - Wishlist - Coupon Function | tanha-coder
    
    * ADD | Add VNPay Checkout Function | tanha-coder
    
    ---------
    
    Co-authored-by: tanha-coder <hanhuttan2004@gmail.com>

--- Server-side/utils/validateCouponFields.ts
+++
@@ -1,9 +0,0 @@
-import { ICoupon } from '../models/couponsModel';
-
-// Hàm validate các trường bắt buộc của coupon
-export const validateCouponFields = (coupon: Partial<ICoupon>): string | null => {
-    if (!coupon.code || !coupon.discountType || !coupon.discountValue || !coupon.startDate || !coupon.endDate) {
-        return 'Missing required fields';
-    }
-    return null;
-};
---
+++ sever-side/dist/routes/payment.routers.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const payment_controller_1 = require("../controllers/payment.controller");
+const auth_middleware_1 = require("../middlewares/auth.middleware");
+const router = (0, express_1.Router)();
+router.post('/create-vnpay-payment', auth_middleware_1.verifyToken, payment_controller_1.createVNPayPayment);
+router.get('/check-payment-vnpay', payment_controller_1.checkVNPayReturn);
+exports.default = router;

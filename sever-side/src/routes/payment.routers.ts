432f0f5fa113c846ea3def17c548eb5110d1f4f5
DamDat-coder <datquoc393@gmail.com>
2025-07-14 21:32:44 +0700

    Dam dat coder (#138)
    * FIX | Fix Home Page Layout With New Server-side | DamDat-coder
    
    * COMMIT | Commit File Stash | DamDat-coder
    
    * ADD | Add Order - Wishlist - Coupon Function | tanha-coder
    
    * ADD | Add VNPay Checkout Function | tanha-coder
    
    * ADD | Add Get Current User | tanha-coder
    
    * REMOVE | Remove node_modules from tracking | DamDat-coder
    
    * FIX | Fix Register Login Function | DamDat-coder
    
    * FIX | Fix Refresh Token Function api.ts | DamDat-coder
    
    * FIX | Fix Create Order Api | DamDat-coder
    
    * ADD | Add Choose Address | DamDat-coder
    
    * FIX | Fix Conflict | DamDat-coder
    
    * ADD | Add Login With Google | DamDat-coder
    
    * FIX | Fix Checkout Address & Address Popup Layout | DamDat-coder
    
    * FIX | Fix Padding Y Login Popup | DamDat-coder
    
    * FIX | Fix Products Page With New Query | DamDat-coder
    
    * FIX | Fix All File Using FetchProducts Function | DamDat-coder
    
    * FIX | Fix CheckAuth Function | DamDat-coder
    
    * ADD | Add ChatBox Ai Component | DamDat-coder
    
    * ADD | Add ZaloPay Payment Function | DamDat-coder
    
    * ADD | Add Notification Call API | DamDat-coder
    
    * ADD | Add Review & Validate Coupons Call API Function | DamDat-coder
    
    * ADD | Add Caterogies Context | DamDat-coder
    
    * CHECK | Check User Client Day 7-9-2025 | DamDat-coder
    
    * ADD | Add Forgot & Reset Password Call API Function | DamDat-coder
    
    * FIX | Fix Logic After Reset Password | DamDat-coder
    
    * ADD | Add Coupons Page & Redirect To Products Page | DamDat-coder
    
    * FIX | Fix Coupons Layout & Update Checkout COD Payment | DamDat-coder
    
    * ADD | Add New Logic Use Coupons At Products Page | DamDat-coder
    
    * REFACTOR | Refactor Coupons Page | DamDat-coder
    
    * RÈACTOR | Refactor Final Version Home Page & Home Components | DamDat-coder
    
    * FIX | Hot Fix Cod Payment | DamDat-coder
    
    ---------
    
    Co-authored-by: tanha-coder <hanhuttan2004@gmail.com>

--- sever-side/src/routes/payment.routers.ts
+++ sever-side/src/routes/payment.routers.ts
@@ -1,14 +1,22 @@
-import { Router } from 'express';
-import { createVNPayPayment, checkVNPayReturn, createZaloPayPayment, checkZaloPayReturn } from '../controllers/payment.controller';
+import { Router } from "express";
+import {
+  createVNPayPayment,
+  checkVNPayReturn,
+  createZaloPayPayment,
+  checkZaloPayReturn,
+  createCodPayment,
+} from "../controllers/payment.controller";
 import { verifyToken } from "../middlewares/auth.middleware";
 const router = Router();
 
 // VNPay
-router.post('/create-vnpay-payment', verifyToken, createVNPayPayment);
-router.get('/check-payment-vnpay', checkVNPayReturn);
+router.post("/create-vnpay-payment", verifyToken, createVNPayPayment);
+router.get("/check-payment-vnpay", checkVNPayReturn);
 
 // ZaloPay
 router.post("/create-payment-zalopay", createZaloPayPayment);
-router.get("/zalopay-callback", checkZaloPayReturn);
+router.get("/check-payment-zalopay", checkZaloPayReturn);
 
+// Cod
+router.post("/create-cod-payment", verifyToken, createCodPayment);
 export default router;

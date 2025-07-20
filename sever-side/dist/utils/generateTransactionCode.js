06beb321a7e1607dd4701f47755b253111af8b69
tanha-coder <tannhutha04@gmail.com>
2025-07-20 13:54:04 +0700

    Tanha coder (#144)
    * ADD | Add Order - Wishlist - Coupon Function | tanha-coder
    
    * ADD | Add VNPay Checkout Function | tanha-coder
    
    * ADD | Add Get Current User | tanha-coder
    
    * FIX | Fix Slug Function | tanha-coder
    
    * FIX | Fix Adress - Lock - Search - Filter User | tanha-coder
    
    * FIX | Filter Products | tanha-coder
    
    * ADD | Add ZaloPay Method Function & SMS User | tanha-coder
    
    * ADD | Add Review API Server | tanha-coder
    
    * ADD | Add Notification Function | tanha-coder
    
    * REFACTOR | Refactor Server-side | tanha-coder
    
    * FIX | Fix Server-side Conflict | tanha-coder
    
    * ADD | Add Final Version | tanha-coder
    
    * FIX | Fix VNPay Payment API | tanha-coder
    
    ---------
    
    Co-authored-by: tanha-coder <hanhuttan2004@gmail.com>
    Co-authored-by: DamDat-coder <datquoc393@gmail.com>

---
+++ sever-side/dist/utils/generateTransactionCode.js
@@ -0,0 +1,35 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateUniqueTransactionCode = generateUniqueTransactionCode;
+const payment_model_1 = __importDefault(require("../models/payment.model"));
+function generateTransactionCode(length = 6) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let code = "";
+    for (let i = 0; i < length; i++) {
+        code += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return code;
+}
+function generateUniqueTransactionCode(prefix) {
+    return __awaiter(this, void 0, void 0, function* () {
+        while (true) {
+            const randomCode = generateTransactionCode();
+            const code = `${prefix}-${randomCode}`;
+            const exists = yield payment_model_1.default.findOne({ transaction_code: code });
+            if (!exists)
+                return code;
+        }
+    });
+}

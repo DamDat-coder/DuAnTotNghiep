b5e51ba4f547258a53534c467bab7eddeae2f33e
DamDat-coder <datquoc393@gmail.com>
2025-07-22 20:10:23 +0700

    Dam dat coder (#149)
    * FIX | Fix Home Page Layout With New Server-side | DamDat-coder
    
    * COMMIT | Commit File Stash | DamDat-coder
    
    * ADD | Add Order - Wishlist - Coupon Function | tanha-coder
    
    * ADD | Add VNPay Checkout Function | tanha-coder
    
    * ADD | Add Get Current User | tanha-coder
    
    * REMOVE | Remove node_modules from tracking | DamDat-coder
    
    * FIX | Fix Register Login Function | DamDat-coder
    
    * FIX | Fix Refresh Token Function api.ts | DamDat-coder
    
    * FIX | Fix Create Order Api | DamDat-coder
    
    * ADD | Add Choose Address | DamDat-coder
    
    * FIX | Fix Conflict | DamDat-coder
    
    * ADD | Add Login With Google | DamDat-coder
    
    * FIX | Fix Checkout Address & Address Popup Layout | DamDat-coder
    
    * FIX | Fix Padding Y Login Popup | DamDat-coder
    
    * FIX | Fix Products Page With New Query | DamDat-coder
    
    * FIX | Fix All File Using FetchProducts Function | DamDat-coder
    
    * FIX | Fix CheckAuth Function | DamDat-coder
    
    * ADD | Add ChatBox Ai Component | DamDat-coder
    
    * ADD | Add ZaloPay Payment Function | DamDat-coder
    
    * ADD | Add Notification Call API | DamDat-coder
    
    * ADD | Add Review & Validate Coupons Call API Function | DamDat-coder
    
    * ADD | Add Caterogies Context | DamDat-coder
    
    * CHECK | Check User Client Day 7-9-2025 | DamDat-coder
    
    * ADD | Add Forgot & Reset Password Call API Function | DamDat-coder
    
    * FIX | Fix Logic After Reset Password | DamDat-coder
    
    * ADD | Add Coupons Page & Redirect To Products Page | DamDat-coder
    
    * FIX | Fix Coupons Layout & Update Checkout COD Payment | DamDat-coder
    
    * ADD | Add New Logic Use Coupons At Products Page | DamDat-coder
    
    * REFACTOR | Refactor Coupons Page | DamDat-coder
    
    * RÈACTOR | Refactor Final Version Home Page & Home Components | DamDat-coder
    
    * FIX | Hot Fix Cod Payment | DamDat-coder
    
    * FIX | Hot Fix Cart Page | DamDat-coder
    
    * ADD | Add Copy Button & Fix Header Performance | DamDat-coder
    
    * REFACTOR | Refactor Final Version Products Page & Products Components | DamDat-coder
    
    * REFACTOR | Refactor Final Version Detail Page & Detail Components | DamDat-coder
    
    * REFACTOR | Refactor Final Version Cart Page & Cart Components | DamDat-coder
    
    * REFACTOR | Refactor Final Version Checkout Page & Checkout Components | DamDat-coder
    
    * REFACTOR | Refactor Final Version Core Components | DamDat-coder
    
    * ADD | Add New Review Logic Call Api | DamDat-coder
    
    * FIX | Fix Cod & VNPay Payment | DamDat-coder
    
    ---------
    
    Co-authored-by: tanha-coder <hanhuttan2004@gmail.com>

--- client-side/src/components/Core/Layout/Header/LookupMenu/SearchSuggestions.tsx
+++ client-side/src/components/Core/Layout/Header/LookupMenu/SearchSuggestions.tsx
@@ -1,36 +1,35 @@
-// src/components/Core/Popups/LookupMenu/SearchSuggestions.tsx
-"use client";
+import { useRouter } from "next/navigation";
 
-import React from "react";
-
-interface SearchSuggestionsProps {
-  suggestions: string[];
-  handleSuggestionClick: (suggestion: string) => void;
-  onClick: (suggestion: string) => void;
+interface Suggestion {
+  name: string;
+  id: string;
 }
 
-export default function SearchSuggestions({
-  suggestions,
-  handleSuggestionClick,
-}: SearchSuggestionsProps) {
+interface SearchSuggestionsProps {
+  suggestions: Suggestion[];
+  onClick: (suggestion: Suggestion) => void;
+}
+
+export default function SearchSuggestions({ suggestions, onClick }: SearchSuggestionsProps) {
+  const router = useRouter();
+
   if (suggestions.length === 0) return null;
 
   return (
     <div className="flex flex-col gap-6">
       <h2 className="text-[1.125rem] text-[#687176]">Gợi ý hàng đầu</h2>
       <div className="flex flex-col gap-2">
-        {suggestions.map((suggestion, index) => (
-          <a
-            key={index}
-            href="#"
-            className="text-base text-black py-1 rounded cursor-pointer"
-            onClick={(e) => {
-              e.preventDefault();
-              handleSuggestionClick(suggestion);
+        {suggestions.map((suggestion) => (
+          <div
+            key={suggestion.id}
+            className="text-base text-black py-2 px-3 rounded cursor-pointer hover:shadow-lg hover:bg-gray-100"
+            onClick={() => {
+              onClick(suggestion);
+              router.push(`/products/${suggestion.id}`);
             }}
           >
-            {suggestion}
-          </a>
+            {suggestion.name}
+          </div>
         ))}
       </div>
     </div>

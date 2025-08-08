4d2a5a2c07834d9b8e8f54972c42a8df24189015
DamDat-coder <datquoc393@gmail.com>
2025-08-08 14:31:29 +0700

    FĨX | Fix Cart Context To Deloy Website | DamDat-coder

--- sever-side/src/index.ts
+++ sever-side/src/index.ts
@@ -1,11 +1,20 @@
 import app from "./app";
 import connectDB from "./config/db";
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+
+const envPath = path.resolve(__dirname, "../env");
+dotenv.config({ path: envPath });
+
 const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Kết nối MongoDB thất bại:", err);
+    process.exit(1);
   });
-});

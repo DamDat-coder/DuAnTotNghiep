eafacf0788d2fe226c069fefbb45f3cdb7f7615f
Trần Văn Công <149676123+congtran203@users.noreply.github.com>
2025-08-12 15:47:06 +0700

    Cong tran (#174)
    * ADD | Add Order - Wishlist - Coupon Function | tanha-coder
    
    * ADD | Add VNPay Checkout Function | tanha-coder
    
    * BUILD | Build admin user, new, coupon | CongTran
    
    * FIX | Fix Conflict | CongTran
    
    * FIX | Fix admin | CongTran
    
    * FIX | Fix admin | CongTran
    
    * BUILD | Build Order Client | CongTran
    
    * FIX | Fix admin new | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | Cong Tran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | Fix Address Api | CongTran
    
    * FIX | CongTran
    
    ---------
    
    Co-authored-by: tanha-coder <hanhuttan2004@gmail.com>
    Co-authored-by: DamDat-coder <datquoc393@gmail.com>

--- sever-side/dist/index.js
+++ sever-side/dist/index.js
@@ -6,10 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const db_1 = __importDefault(require("./config/db"));
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const path_1 = __importDefault(require("path"));
+const envPath = path_1.default.resolve(__dirname, "../env");
+dotenv_1.default.config({ path: envPath });
 const PORT = process.env.PORT || 3000;
-(0, db_1.default)().then(() => {
+(0, db_1.default)()
+    .then(() => {
     app_1.default.listen(PORT, () => {
         console.log(`🚀 Server running at http://localhost:${PORT}`);
     });
+})
+    .catch((err) => {
+    console.error("❌ Kết nối MongoDB thất bại:", err);
+    process.exit(1);
 });

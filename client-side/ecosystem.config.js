236aa937cb0f3a4e847985df7ba970dc038c50fa
nn2301 <nn23012001@gmail.com>
2025-08-15 10:23:28 +0700

    FIX | FE ecosystem, BE ecosystem

--- client-side/ecosystem.config.js
+++ client-side/ecosystem.config.js
@@ -8,12 +8,7 @@ module.exports = {
       args: "start -p 3300",
       env: {
         NODE_ENV: "production",
-<<<<<<< HEAD
         NEXT_PUBLIC_API_URL: "http://api.styleforyou.online",
-=======
-        // nhớ dùng HTTPS
-        NEXT_PUBLIC_API_URL: "https://api.styleforyou.online",
->>>>>>> 26c33e0f889b1f3595e0a2ba18d8d7b249b66ca8
         NEXT_PUBLIC_GOOGLE_CLIENT_ID:
           "995974862597-d2ab3adri4496s828b088pj94l4b9ofn.apps.googleusercontent.com",
         NEXT_PUBLIC_DISABLE_FAST_REFRESH: "true",

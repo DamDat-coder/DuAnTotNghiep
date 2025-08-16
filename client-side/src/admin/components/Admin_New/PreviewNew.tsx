2cc84c56691f76f60b98f477774a550c265f6cd7
Trần Văn Công <149676123+congtran203@users.noreply.github.com>
2025-08-16 08:43:23 +0700

    Cong tran (#187)
    * ADD | Add Order - Wishlist - Coupon Function | tanha-coder
    
    * ADD | Add VNPay Checkout Function | tanha-coder
    
    * BUILD | Build admin user, new, coupon | CongTran
    
    * FIX | Fix Conflict | CongTran
    
    * FIX | Fix admin | CongTran
    
    * FIX | Fix admin | CongTran
    
    * BUILD | Build Order Client | CongTran
    
    * FIX | Fix admin new | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | Cong Tran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | Fix Address Api | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    * FIX | CongTran
    
    ---------
    
    Co-authored-by: tanha-coder <hanhuttan2004@gmail.com>
    Co-authored-by: DamDat-coder <datquoc393@gmail.com>

--- client-side/src/admin/components/Admin_New/PreviewNew.tsx
+++ client-side/src/admin/components/Admin_New/PreviewNew.tsx
@@ -31,7 +31,7 @@ export default function PreviewNew({
   return (
     <div className="fixed inset-0 z-50 bg-black bg-opacity-40 flex items-center justify-center p-4">
       <div className="bg-white rounded-lg shadow-lg max-w-2xl w-full relative">
-        <div className="max-h-[80vh] overflow-y-auto p-8">
+        <div className="max-h-[80vh] overflow-y-auto p-8 scroll-hidden">
           <button
             onClick={onClose}
             className="absolute top-4 right-4 text-gray-500 hover:text-black"

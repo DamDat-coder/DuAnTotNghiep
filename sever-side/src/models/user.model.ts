1dfe55db0ef1016ad2a78237afc1f25e0888e1b6
tanha-coder <hanhuttan2004@gmail.com>
2025-08-18 21:56:27 +0700

    BUILD | Build AI BE | tanha-coder

--- sever-side/src/models/user.model.ts
+++ sever-side/src/models/user.model.ts
@@ -19,7 +19,7 @@ export interface IUser extends Document {
   password: string;
   name: string;
   addresses: Types.DocumentArray<IAddress>;
-  phone: string | null; 
+  phone: string | null;
   role: "user" | "admin";
   is_active: boolean;
   refreshToken?: string | null;
